753a76b0f15deb14e8917f049e153b8dd91bcbd9
Michael Bisgaard Olesen <michaelbisgaardo@gmail.com>
2023-05-22 09:25:08 +0200

    Fix

--- index.js
+++ index.js
@@ -3,19 +3,19 @@ const {EOL} = require('os');
 const {readFileSync, writeFileSync} = require('fs');
 const {compute} = require('./lib');
 
-const delimiter = 'EOT';
+const DELIMITER = 'EOT';
+const SHELL = '/bin/bash'; // TODO determine based on OS ('https://github.com/actions/runner-images/blob/ubuntu22/20230507.1/images/win/Windows2019-Readme.md#shells' etc.)
 
 const {env} = process;
-console.debug(env);
 const outputFile = env.GITHUB_OUTPUT;
-const outputs = compute(env, 'INPUT_');
+const outputs = compute(env, 'INPUT_', SHELL);
 
 // Write result as an output (assuming that it's multiline).
 function toOutput(key, delimiter, value) {
     return `${key}<<${delimiter}${EOL}${value}${EOL}${delimiter}${EOL}`;
 }
 
-const res = Object.entries(outputs).map(([name, val]) => toOutput(name, delimiter, val)).join('');
+const res = Object.entries(outputs).map(([name, val]) => toOutput(name, DELIMITER, val)).join('');
 try {
     writeFileSync(outputFile, res, {encoding: 'utf8'});
     console.log(`DEBUG: outputFile contents=${readFileSync(outputFile)}`);
