f11e1bca38b8f113de12d6e5376fc3ce1221a2b9
Michael Bisgaard Olesen <michaelbisgaardo@gmail.com>
2023-05-24 22:54:35 +0200

    Improve readability
    Various minor improvements to code readability.

--- lib.js
+++ lib.js
@@ -1,9 +1,5 @@
 const { exec } = require("child_process");
 
-function indent(str) {
-  return str.replaceAll(/^/gm, "  ");
-}
-
 function evalCmd(cmd, input) {
   return new Promise((resolve, reject) => {
     const process = exec(cmd, (err, stdout, stderr) => {
@@ -21,6 +17,10 @@ function evalCmd(cmd, input) {
 }
 
 async function evalShell(shell, script) {
+  function indent(str) {
+    return str.replaceAll(/^/gm, "  ");
+  }
+
   // Assuming that the shell supports flag '-s' for running stdin.
   try {
     return await evalCmd(`${shell} -s`, script);
@@ -39,20 +39,19 @@ function resolveEntries(envObj, prefix) {
       expr,
     }));
 }
-function escapeDoubleQuotesInDoubleQuotes(str) {
+function wrapInDoubleQuotes(str) {
   // Double quotes are escaped unless they occur inside a substitution.
-  // As substitutions can be nested we need to partially parse the expression and track open substitutions
-  // to determine whether a given double quote should be escaped.
+  // As substitutions can be nested we need to (partially) parse the expression to track open substitutions.
+  // A given double quote should only be escaped if the stack of open substitutions is empty.
   // The insertion of backspaces is done by flushing the input string into the result whenever a backspace is to be written.
 
   const closingSymbols = []; // stack of symbols that close the currently open substitutions
   let prev = null;
   let beforePrev = null;
-  let res = "";
-  let fromExprIdx = 0;
+  let nextFlushIdx = 0;
+  let escapedStr = "";
   for (const [idx, cur] of Array.from(str).entries()) {
-    const currentClosingSymbol =
-      closingSymbols.length && closingSymbols[closingSymbols.length - 1];
+    const currentClosingSymbol = closingSymbols.length && closingSymbols[closingSymbols.length - 1];
     if (prev !== "\\" && currentClosingSymbol === cur) {
       // Found matching (unescaped) end symbol to the topmost open substitution: popping the substitution from the stack.
       closingSymbols.pop();
@@ -72,15 +71,16 @@ function escapeDoubleQuotesInDoubleQuotes(str) {
     } else if (cur === `"` && currentClosingSymbol !== "'") {
       // Found (escaped or unescaped) double quote not inside single quotes.
       if (!closingSymbols.length) {
-        // Double quote is not inside a substitution: escape by flushing 'expr' into 'res' up to current 'idx' before writing the escape symbol '\'.
+        // Double quote is not inside a substitution:
+        // escape by flushing 'str' into 'escapedStr' up to current 'idx' before writing the escape symbol '\'.
         // The '"' will be written on the next flush.
-        res += str.slice(fromExprIdx, idx);
-        res += "\\";
+        escapedStr += str.slice(nextFlushIdx, idx);
+        escapedStr += "\\";
         if (prev === "\\") {
-          // If the quote was already escaped we need to add a full escaped backslash.
-          res += "\\";
+          // If the quote was already escaped we need to add a fully escaped backslash.
+          escapedStr += "\\";
         }
-        fromExprIdx = idx;
+        nextFlushIdx = idx;
       } else if (prev !== "\\") {
         // Double quote is inside an expansion and not escaped. To be closed by a matching (unescaped) double quote.
         // Found unescaped double quote inside an expansion. To be closed by a matching (unescaped) double quote.
@@ -91,14 +91,15 @@ function escapeDoubleQuotesInDoubleQuotes(str) {
     prev = cur;
   }
   // Final flush of 'expr'.
-  res += str.slice(fromExprIdx);
-  return res;
+  escapedStr += str.slice(nextFlushIdx);
+  return `"${escapedStr}"`;
 }
 
-function escapeSingleQuotesWithinSingleQuotes(str) {
+function wrapInSingleQuotes(str) {
   // Single quotes are escaped by adding an ending single quote,
   // then an escaped single quote and a re-opening single quote.
-  return str.replaceAll(`'`, `'\\''`);
+  const escapedStr = str.replaceAll(`'`, `'\\''`);
+  return `'${escapedStr}'`;
 }
 
 function isValidBashVariableName(name) {
@@ -111,14 +112,14 @@ async function compute(entries, shell) {
   let shellAssignments = "";
   for (const { name, expr } of entries) {
     // Wrap expr in double quotes to support (and preserve) spacing.
-    const echo = `echo -n "${escapeDoubleQuotesInDoubleQuotes(expr)}"`;
+    const echo = `echo -n ${wrapInDoubleQuotes(expr)}`;
     const script = shellAssignments + echo;
     const output = await evalShell(shell, script);
     outputs[name] = output;
     // If 'name' is a valid Bash variable, append assignment of the value for use in subsequent evaluations.
     // Output is wrapped in single quotes to prevent expansion of any expressions in the output.
     if (isValidBashVariableName(name)) {
-      shellAssignments += `${name}='${escapeSingleQuotesWithinSingleQuotes(output)}'\n`;
+      shellAssignments += `${name}=${wrapInSingleQuotes(output)}\n`;
     }
   }
   return outputs;
